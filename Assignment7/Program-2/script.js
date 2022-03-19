86f39acf0b8308ce202d38cf204bcc54fd7b8eec
BoraElena <100995175+BoraElena@users.noreply.github.com>
2022-03-19 17:57:13 +0200

    Assignment7

---
+++ Assignment7/Program-2/script.js
@@ -0,0 +1,12 @@
+// let operator = prompt ("Operator?")
+// let firstNumber = prompt("Tell me the first number?")
+// let secondNumber = prompt("Tell me the second number?")
+//     console.log( `${firstNumber}${operator}${secondNumber}`)
+// //  = ${result} 
+// // }-
+let operator, firstNumber, secondNumber;
+operator = prompt ("Operator?")
+firstNumber = prompt ("Tell me the first number?")
+secondNumber = prompt("Tell me the second number?")
+let result = Number firstNumber 
+console.log(`${firstNumber}${operator}${secondNumber} = ${result}`)
