30dee2feb9955670878974406bb360f30044dbd2
BoraElena <100995175+BoraElena@users.noreply.github.com>
2022-03-19 17:59:59 +0200

    Update script.js

--- Assignment7/Program-2/script.js
+++ Assignment7/Program-2/script.js
@@ -8,5 +8,5 @@ let operator, firstNumber, secondNumber;
 operator = prompt ("Operator?")
 firstNumber = prompt ("Tell me the first number?")
 secondNumber = prompt("Tell me the second number?")
-let result = Number firstNumber 
+let result =
 console.log(`${firstNumber}${operator}${secondNumber} = ${result}`)
